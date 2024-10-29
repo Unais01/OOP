0ba8606409ce0681e00149ec62ebb7529ac6f61c
Unais <unaissk1234@gmail.com>
2024-10-29 17:53:28 +0530

    inheritance updated

--- inheritance.cpp
+++ inheritance.cpp
@@ -34,6 +34,28 @@ public:
         ownerName = ownername;
     }
 };
+class GamingChannel : public Youtube
+{
+private:
+    string favouriteGame;
+
+public:
+    GamingChannel(string channelname, string ownername, int subscriber, string favEnv) : Youtube(channelname, ownername, subscriber)
+    {
+        favouriteGame = favEnv;
+    }
+    void ChannelDetails()
+    {
+        cout << "Channel Name :" << channelName << endl;
+        cout << "Owner Name :" << ownerName << endl;
+        cout << "Subsciber :" << subscriberCount << endl;
+        cout << "Favourite Game : " << favouriteGame << endl;
+        for (auto itr : videos)
+        {
+            cout << itr << " | ";
+        }
+    }
+};
 
 class CodingChannel : public Youtube
 {
@@ -67,10 +89,14 @@ int main()
 
     // Youtube CodingChannel02("FreeCodeCamp", "Community Channel", 0);
     CodingChannel c1("Code With Harry", "Haris Ali Khan", 0, "Javascript");
+    GamingChannel c2("Techno Games", "John", 0, "BGMI");
     // c1.Subscribe();
-    vector<string> titleOfVideo = {"C++ in one shot", "100 days of Javascript", "React BootCamp"};
-    c1.AddVideos(titleOfVideo);
+    vector<string> titleOfVideo1 = {"C++ in one shot", "100 days of Javascript", "React BootCamp"};
+    vector<string> titleOfVideo2 = {"BGMI Streaming", "Clash of Clan War Times", "Legacy Games:Prince of Persia"};
+    c1.AddVideos(titleOfVideo1);
+    c2.AddVideos(titleOfVideo2);
     c1.ChannelDetails();
+    c2.ChannelDetails();
 
     return 0;
 }
